3bcf140a657605a355e1527661425488eacf0fa6
onaynov.dn <danil@onyanov.ru>
2016-08-05 17:19:20 +0300

    update addActivity

--- app/src/main/java/ru/onyanov/yesorno/AddActivity.java
+++ app/src/main/java/ru/onyanov/yesorno/AddActivity.java
@@ -2,60 +2,39 @@ package ru.onyanov.yesorno;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.Button;
+import android.util.Log;
 import android.widget.RadioButton;
-import android.widget.Spinner;
 import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class AddActivity extends Activity {
 
-	private Button button;
-	private TextView questionView;
-	private TextView commentView;
-	private Spinner answerView;
-	private String question;
-	private String comment;
-	private int answer = 0;
-	private RadioButton answerYesView;
+	private static final String TAG = "AddActivity";
+
+	@BindView(R.id.editQuestion) TextView questionView;
+	@BindView(R.id.editComment) TextView commentView;
+	@BindView(R.id.radioButtonYes) RadioButton answerYesView;
+
+	@OnClick(R.id.add) void submit() {
+		String question = questionView.getText().toString();
+		String comment = commentView.getText().toString();
+		boolean isCorrect = answerYesView.isChecked();
+
+		if (question.equals("")) {
+			questionView.requestFocus();
+		} else {
+			Log.d(TAG, "onClick: [" + question + ", " + comment + ", " + isCorrect + "]");
+		}
+		// TODO call server...
+	}
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_add);
-		
-		button = (Button) findViewById(R.id.add);
-		questionView = (TextView) findViewById(R.id.editQuestion);
-		answerYesView = (RadioButton) findViewById(R.id.radioButtonYes);
-		RadioButton answerNoView = (RadioButton) findViewById(R.id.radioButtonNo);
-		commentView = (TextView) findViewById(R.id.editComment);
-		
-		button.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View arg0) {
-				question = (String) questionView.getText().toString();
-				comment = (String) commentView.getText().toString();
-				
-				if (question.equals("")) {
-					questionView.requestFocus();
-				} else {
-					
-				}
-			}
-		});
-		
-		answerView.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-		    public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) {
-		        answer = pos;
-		    }
-		    public void onNothingSelected(AdapterView<?> parent) {
-		    }
-
-		});
-
-
-		//answerNoView.setOnCheckedChangeListener(answerListener);
+		ButterKnife.bind(this);
 	}
-	
 }
